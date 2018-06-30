0739b90d9f38e03f580d528acd3220220c940242
Isaac Villicana <villicana.i@gmail.com>
2018-06-30 01:04:20 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ require './config/environment'
 class ApplicationController < Sinatra::Base
 
   configure do
-    enable :sessions
+    enable :sessions unless test?
     set :session_secret, "$3kR*Tk3y5"
     set :public_folder, 'public'
     set :views, 'app/views'
